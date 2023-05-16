19fdb929fc2401126681b89fcd5fbc9b27e5516c
jack <jrfisch95@gmail.com>
2023-05-16 10:34:50 -0500

    add lato font family. being styling login page.

---
+++ apps/crx/src/pages/Login.svelte
@@ -0,0 +1,94 @@
+<div
+  class="flex px-4 md:px-8 pt-16 pb-8 w-full overflow-auto flex-1 gap-3 items-center flex-col justify-center"
+>
+  <div class="flex flex-col justify-center items-center">
+    <h2 class="typography h2 primaryg">Welcome back!</h2>
+    <p class="typography gray body">
+      It's so good to see you. Seriously 🧡
+    </p>
+  </div>
+  <div
+    class="divider font-bold whitespace-nowrap before:bg-gray-200 after:bg-gray-200 divider-vertical w-full"
+  />
+  <form class="w-full gap-4 mb-4 flex flex-col items-center">
+    <input type="hidden" name="inviteID" value="undefined" />
+    <div
+      class="relative lg-textbox flex flex-row justify-center w-full white"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 62 64"
+        width="16"
+        height="16"
+        ><g clip-path="url(#clip0)"
+          ><path
+            fill="currentColor"
+            fill-rule="evenodd"
+            d="M0 57.607c2.904-11.872 15.55-20.818 30.699-20.818 15.128 0 27.774 8.946 30.678 20.818-19.926 8.324-40.357 8.72-61.377 0zM30.699 0c9.216 0 16.686 7.478 16.686 16.69 0 9.212-7.47 16.692-16.686 16.692-9.216 0-16.707-7.48-16.707-16.692S21.482 0 30.7 0z"
+            clip-rule="evenodd"
+          /></g
+        ><defs
+          ><clipPath id="clip0"
+            ><path fill="#fff" d="M0 0h61.377v63.999H0z" /></clipPath
+          ></defs
+        ></svg
+      >
+      <input
+        required=""
+        placeholder="Username"
+        name="email"
+        class="svelte-60zm7w"
+        type="email"
+      />
+    </div>
+    <div
+      class="relative lg-textbox flex flex-row justify-center w-full white"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 65 64"
+        width="16"
+        height="16"
+        ><g clip-path="url(#clip0)"
+          ><path
+            fill="currentColor"
+            d="M38.53 40.718c0-1.605-.588-2.976-1.762-4.112-1.175-1.136-2.593-1.705-4.253-1.705-1.66 0-3.077.569-4.252 1.705-1.175 1.136-1.762 2.507-1.762 4.112 0 1.121.297 2.136.893 3.045A6.184 6.184 0 0029.79 45.9l-3.242 10.407c-.157.455-.078.879.235 1.273.313.393.72.59 1.221.59h9.022c.501 0 .908-.197 1.222-.59.313-.394.391-.818.235-1.273L35.24 45.9a6.184 6.184 0 002.396-2.136 5.438 5.438 0 00.893-3.044zM20.487 29.084h24.057V20.36c0-3.212-1.175-5.953-3.524-8.226-2.35-2.272-5.184-3.408-8.505-3.408-3.32 0-6.155 1.136-8.504 3.409-2.35 2.272-3.524 5.014-3.524 8.225v8.725zm39.093 4.363v26.177c0 1.212-.439 2.241-1.316 3.09-.877.848-1.942 1.272-3.195 1.272H9.961c-1.253 0-2.318-.424-3.195-1.272-.877-.849-1.315-1.878-1.315-3.09V33.447c0-1.212.438-2.242 1.315-3.09.877-.848 1.942-1.273 3.195-1.273h1.504V20.36c0-5.575 2.068-10.361 6.202-14.36 4.135-4 9.084-6 14.848-6 5.764 0 10.713 2 14.848 6 4.135 3.999 6.203 8.785 6.203 14.36v8.725h1.503c1.253 0 2.318.425 3.195 1.273.877.848 1.316 1.878 1.316 3.09z"
+          /></g
+        ><defs
+          ><clipPath id="clip0"
+            ><path
+              fill="#fff"
+              d="M0 0h64v64H0z"
+              transform="translate(.377)"
+            /></clipPath
+          ></defs
+        ></svg
+      >
+      <input
+        required=""
+        placeholder="Password"
+        name="password"
+        class="svelte-60zm7w"
+        type="password"
+      />
+    </div>
+    <div class="flex flex-col justify-between gap-6 w-full items-center">
+      <button
+        type="submit"
+        class="btn gap-2 w-full btn-md btn-contained primary"
+      >
+        <span class="lg-btn-text">Login</span>
+      </button>
+      <div class="flex flex-row gap-2 justify-start items-center">
+        <input type="checkbox" name="keep" id="keep" />
+        <label for="keep"
+          ><p class="typography subheadline">
+            Keep me logged in
+          </p></label
+        >
+      </div>
+    </div>
+  </form>
+</div>
